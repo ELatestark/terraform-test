4a9dd96a5c2429377c549cad1646f60c3e67171c
ELatestark <ravenjkeee@gmail.com>
2021-09-26 23:00:28 +0700

    added separate .tf

--- 6.tf
+++ 6.tf
@@ -91,6 +91,7 @@ resource "aws_instance" "jumpbox-instance" {
               #!/bin/bash
               adduser teacher
               usermod -a -G wheel teacher
+              echo 'teacher        ALL=(ALL)       NOPASSWD: ALL' >> /etc/sudoers
               mkdir /home/teacher/.ssh
               chown teacher:teacher /home/teacher/.ssh
               chmod 700 /home/teacher/.ssh
